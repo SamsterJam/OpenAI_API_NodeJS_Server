06164cecda2e53fb0b4097c8bbfba88e94e62aad
Mossy7777 <nomedidio@gmail.com>
2022-12-26 14:18:21 -0500

    Finished front-end rework + quality of life update

--- public/js/apiCaller.js
+++ public/js/apiCaller.js
@@ -1,21 +1,15 @@
-/*
-function toggleShowPass(){
-    var inpass = document.getElementById("api-key");
-    if (inpass.type === "password") {
-        inpass.type = "text";
-        document.getElementById("eye-icon").className = "fa-regular fa-eye";
-        document.getElementById("view-button").style = "border-radius: 45px; background: #121212; box-shadow: inset 5px 5px 10px #0b0b0b, inset -5px -5px 10px #191919;";
-    } else {
-        inpass.type = "password";
-        document.getElementById("eye-icon").className = "fa-regular fa-eye-slash";
-        document.getElementById("view-button").style = "border-radius: 45px; background: linear-gradient(145deg, #131313, #101010); box-shadow:  5px 5px 10px #0b0b0b, -5px -5px 10px #191919;";
-    }
-}
-*/
 
-/*
+function clearGhostText(){
+    document.getElementById("ghost-text").value='';
+}
+
+document.getElementById('prompt').addEventListener('keydown', function(e) {
+	if(e.code == 'Enter' && e.ctrlKey) {
+		apiCallTestSubmit()
+	}
+});
 //Temerature Slider
-var slider1 = document.getElementById("selected-temp");
+var slider1 = document.getElementById("temperature-selection");
 var output1  = document.getElementById("selected-temp-value");
 output1.innerHTML = slider1.value;
 slider1.oninput = function() {
@@ -23,7 +17,7 @@ slider1.oninput = function() {
 }
 
 //Length Slider
-var slider2 = document.getElementById("selected-length");
+var slider2 = document.getElementById("length-selection");
 var output2  = document.getElementById("selected-length-value");
 output2.innerHTML = slider2.value;
 slider2.oninput = function() {
@@ -31,7 +25,7 @@ slider2.oninput = function() {
 }
 
 //Freq Slider
-var slider3 = document.getElementById("selected-frequency");
+var slider3 = document.getElementById("frequency-selection");
 var output3  = document.getElementById("selected-frequency-value");
 output3.innerHTML = slider3.value;
 slider3.oninput = function() {
@@ -39,32 +33,42 @@ slider3.oninput = function() {
 }
 
 //Pres Slider
-var slider4 = document.getElementById("selected-presence");
+var slider4 = document.getElementById("presence-selection");
 var output4  = document.getElementById("selected-presence-value");
 output4.innerHTML = slider4.value;
 slider4.oninput = function() {
     output4.innerHTML = slider4.value;
 }
 
-*/
+
+
+function hideKey(){
+    document.getElementById("API-Key").type="password";
+}
+
+function showKey(){
+    document.getElementById("API-Key").type="text";
+}
 
 function apiCallTestSubmit(){
     document.getElementById("prompt").disabled = true;
     document.getElementById("prompt").style="background-color: #1d1e23;";
     document.getElementsByClassName("loader")[0].style="visibility: visible";
+    var button = document.getElementById("submit-button");
+    button.disabled = true;
+    button.style="background: linear-gradient(174deg, #6c56b3 0%, #483683 100%);";
 
-    /*
+    
     // Get the values of the input fields
-    var model = document.getElementById("selected-model");
-    var modelValue = model.value;
+    var model = document.getElementById("model-selection");
     var modelChoice = model.options[model.selectedIndex].text
 
-    var temp = document.getElementById("selected-temp").value;
-    var length = document.getElementById("selected-length").value;
-    var frequency = document.getElementById("selected-frequency").value;
-    var presence = document.getElementById("selected-presence").value;
+    var temp = document.getElementById("temperature-selection").value;
+    var length = document.getElementById("length-selection").value;
+    var frequency = document.getElementById("frequency-selection").value;
+    var presence = document.getElementById("presence-selection").value;
     var prompt = document.getElementById("prompt").value.toString();
-    var apiKey = document.getElementById("api-key").value.toString();
+    var apiKey = document.getElementById("API-Key").value.toString();
     // Get the values of the other input fields
 
     fetch('/apiCall', {
@@ -77,26 +81,34 @@ function apiCallTestSubmit(){
     })
     .then(response => response.json())
     .then(response => updateWithData(response))
-    */
+    
 
 };
 
-/*
-function updateWithData(response){
-    if(response.choices[0].text == '' || response.choices[0] == 'API-ERROR'){
-        document.getElementById("prompt").value += '[NO COMPLEATION]';
-        document.getElementById("prompt").style="background: #1C1515;";
 
+function updateWithData(response){
+    if(response.choices[0].text == ''){
+        document.getElementById("ghost-text").value = document.getElementById("prompt").value + ' [NO COMPLEATION]';
+    }else if(response.choices[0].text == 'API-ERROR'){
+        document.getElementById("ghost-text").value = document.getElementById("prompt").value + ' [API ERROR]';
     }else{
         document.getElementById("prompt").value += response.choices[0].text;
-        document.getElementById("prompt").style="#1d1d1d";
     }
 
-    document.getElementById("prompt").disabled = false;
-    document.getElementsByClassName("lds-facebook")[0].style="visibility: hidden";
+    var prompt = document.getElementById("prompt");
+    prompt.disabled = false;
+    prompt.style="background-color: #2b2f38;";
+    prompt.scrollTop = prompt.scrollHeight;
+
+    document.getElementsByClassName("loader")[0].style="visibility: hidden";
+
+    var button = document.getElementById("submit-button");
+    button.disabled=false;
+    button.style="background: linear-gradient(174deg, #9a7bfd 0%, #7558d2 100%);";
+    
 }
 
-*/
+
 
 
 
@@ -124,4 +136,4 @@ function parseQuery(str) {
 var query = window.location.search
 var keyInfo = parseQuery(query);
 
-document.getElementById('api-key').value = keyInfo.APIkeyid;
+document.getElementById('API-Key').value = keyInfo.APIkeyid;
